768d1fecb1a2bb44e2537498abd1a7f7ed210584
Preethuakhilraj <46960642+Preethuakhilraj@users.noreply.github.com>
2024-06-12 20:32:07 +0530

    commit
    commit

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
@@ -8,26 +11,33 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+const upload = multer({ dest: 'uploads/' });
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER, 
+    user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
-app.post('/send-email', async (req, res) => {
+app.post('/send-email', upload.array('attachments'), async (req, res) => {
   const { to, from, subject, text } = req.body;
   const recipients = Array.isArray(to) ? to : [to];
+  const attachments = req.files ? req.files.map(file => ({
+    filename: file.originalname,
+    path: file.path
+  })) : [];
 
   try {
     const info = await transporter.sendMail({
       from: from,
-      to: recipients.join(', '), 
+      to: recipients.join(', '),
       subject: subject,
       text: text,
+      attachments: attachments,
     });
 
     console.log('Message sent: %s', info.messageId);
@@ -35,29 +45,11 @@ app.post('/send-email', async (req, res) => {
   } catch (error) {
     console.error('Error sending email:', error);
     res.status(500).json({ message: 'Error sending email.' });
+  } finally {
+
+    req.files && req.files.forEach(file => fs.unlinkSync(file.path));
   }
 });
-
-// optional:Test email sending directly without frontend
-async function main() {
-  try {
-    const info = await transporter.sendMail({
-      from: process.env.EMAIL_USER, 
-      to: "preethuakhilraj@gmail.com", 
-      subject: "Test mail", 
-      text: "Test" 
-    });
-
-    console.log("Message sent: %s", info.messageId);
-  } catch (error) {
-    console.error("Error sending email: ", error);
-  }
-}
-
-
-main().catch(console.error);
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, (err) => {
   if (err) {
@@ -65,3 +57,4 @@ app.listen(port, (err) => {
   }
   console.log(`Server is listening on port ${port}`);
 });
+
